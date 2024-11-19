78a6229fb63442bedc48e6263bd0d828ca1d0a7e
rafael556 <rafael7jf@hotmail.com>
2024-11-19 15:33:34 -0300

    feat: add query bus logic

---
+++ src/core/shared/application/query.bus.ts
@@ -0,0 +1,22 @@
+import { QueryHandler } from './query-handler.interface';
+
+export class QueryBus {
+  private readonly handlers = new Map<string, QueryHandler<any, any>>();
+
+  register<TQuery, TResult>(
+    queryName: string,
+    handler: QueryHandler<TQuery, TResult>,
+  ) {
+    this.handlers.set(queryName, handler);
+  }
+
+  async execute<TQuery, TResult>(query: TQuery): Promise<TResult> {
+    const handler = this.handlers.get(query.constructor.name);
+
+    if (!handler) {
+      throw new Error(`No handler registered for query ${query.constructor.name}`);
+    }
+
+    return handler.execute(query);
+  }
+}
